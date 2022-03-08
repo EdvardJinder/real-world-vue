a3f6c63102a6d88a272c06153d5010c18faf4adb
EdvardJinder <89024464+EdvardJinder@users.noreply.github.com>
2022-03-08 19:01:12 +0100

    1.0

---
+++ src/components/EventCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <span>{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: Object,
+  },
+}
+</script>
+
+<style scoped>
+.card {
+  padding: 20px 30px 20px 30px;
+  border: 2px solid #2c3e50;
+  margin-bottom: 10px;
+}
+</style>
